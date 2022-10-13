8cffa3a4f7c46939374aa9c8daf4308e22b34f8b
Rustam Dzhaksimov <dzhaksimov.rustam@gmail.com>
2022-10-13 15:06:17 +0300

    feat: create project structure and api

--- globexit/src/App.js
+++ globexit/src/App.js
@@ -1,8 +1,8 @@
-import logo from './logo.svg';
 import './App.css';
+import Main from './pages/Main';
 
 function App() {
-
+  return (<Main />);
 }
 
 export default App;
---
+++ globexit/src/services/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios';
+
+export const axiosInstance = axios.create({
+    baseURL: 'http://127.0.0.1:3000/',
+    timeout: 1000,
+});
+  
+export const getAllUsersData = async () => {
+    const response = axiosInstance.get();
+    const UserList = await response;
+
+    return UserList;
+};
+
+export const getFilteredUsersData = async (searchValue) => {
+    const response = axiosInstance.get(`?term=${searchValue}`);
+    const UserList = await response;
+
+    return UserList;
+}
