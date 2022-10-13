2bd68808e1453de552fd22a3d7e554828c13597d
Rustam Dzhaksimov <dzhaksimov.rustam@gmail.com>
2022-10-13 18:59:31 +0300

    feat: add modal window styles, add close button

--- globexit/src/components/Modal/Modal.js
+++ globexit/src/components/Modal/Modal.js
@@ -5,6 +5,7 @@ function Modal({userCardData, closeModal}) {
   return (
     <div className="modal" onClick={closeModal}>
       <div className="modalContent" onClick={event => event.stopPropagation()}>
+        <img className="modalContent__closeBtn" src="img/close.png" alt="close modal" onClick={closeModal} />
         <h3>{userCardData.name}</h3>
         <div className="modalContent__wrapper">
           <div className="modalContent__description">
@@ -23,7 +24,7 @@ function Modal({userCardData, closeModal}) {
           </div>
         </div>
         <div className="modalContent__additionalInfo">
-          <h5>Дополнительная информация</h5>
+          <h4>Дополнительная информация</h4>
           <span>Разработчики используют текст Lorem Ipsum в качестве заполнителя макета
             страницы. Так как дополнительной информации в JSON нет, а адрес нигде не используется
             закинул его сюда: {userCardData.address}
